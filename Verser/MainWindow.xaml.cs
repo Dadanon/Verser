c9c6ed205a1f3bd8f5b3c60507bbb42934fe9f69
Dadanon <52732101+Dadanon@users.noreply.github.com>
2022-09-20 00:07:36 +0300

    Title list updates correctly, poems are deleted correctly

--- Verser/MainWindow.xaml.cs
+++ Verser/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SQLite;
+using System.Collections.ObjectModel;
 
 namespace Verser
 {
@@ -22,19 +23,20 @@ namespace Verser
     public partial class MainWindow : Window
     {
         ApplicationContext db;
+        List<Poem> poems;
         public MainWindow()
         {
             InitializeComponent();
             db = new ApplicationContext();
             if (db != null)
             {
-                List<Poem> poems = db.Poems.ToList();
+                poems = db.Poems.ToList();
                 foreach (Poem poem in poems)
                 {
                     Button n = new Button();
                     n.Margin = new Thickness(10);
                     n.Name = "Button" + poem.Id.ToString();
-                    n.Content = poem.Title;
+                    n.Content = poem.Title + " - " + poem.Author;
                     n.Click += new RoutedEventHandler(this.LinkClick);
                     TitleView.Children.Add(n);
                 }
@@ -52,7 +54,7 @@ namespace Verser
             string newTitle = "";
             string poemId = (sender as Button).Name;
             string search = $"SELECT * FROM poems WHERE Id = {poemId.Substring(6)}";
-            SQLiteConnection m_db = new SQLiteConnection("DefaultConnection");
+            SQLiteConnection m_db = new SQLiteConnection("Data Source = poemsDb.db");
             m_db.Open();
             SQLiteCommand command = new SQLiteCommand(search, m_db);
             SQLiteDataReader reader = command.ExecuteReader();
@@ -70,11 +72,47 @@ namespace Verser
         }
         private void LinkClick(object sender, RoutedEventArgs e)
         {
-            string[] data = SearchDataById(sender, e);
-            PoemWindow ne = new PoemWindow();
-            ne.TitleRow.Text = data[0];
-            ne.TextRow.Text = data[1];
-            OpenClose.ChangeWindow(this, ne);
+            if (this.Cursor != Cursors.Pen)
+            {
+                string[] data = SearchDataById(sender, e);
+                PoemWindow ne = new PoemWindow();
+                ne.TitleRow.Text = data[0];
+                ne.TextRow.Text = data[1];
+                OpenClose.ChangeWindow(this, ne);
+            }
+            else
+            {
+                ButtonDelete_Click(sender, e);
+                this.Cursor = Cursors.Arrow;
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult mbr = MessageBox.Show("Do you want to delete this poem?", "Important Decision", MessageBoxButton.YesNo);
+            switch (mbr)
+            {
+                case MessageBoxResult.Yes:
+                    int poemId;
+                    bool success = int.TryParse((sender as Button).Name.Substring(6), out poemId);
+                    Poem poemsInDb = db.Poems.First(x => x.Id == poemId);
+                    db.Poems.Remove(poemsInDb);
+                    db.SaveChanges();
+                    TitleView.Children.Remove(sender as Button);
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+        }
+        private void ChangeCursor(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Pen;
+        }
+
+        private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (this.Cursor == Cursors.Pen)
+                this.Cursor = Cursors.Arrow;
         }
     }
 }
